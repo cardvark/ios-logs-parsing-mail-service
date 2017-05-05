bdacaf1f7674066cb28db8cd23be2d961991a027
James Wang <endaction@gmail.com>
2017-05-04 18:58:43 -0700

    Send to admins with content

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
+from google.appengine.api import mail
 import webapp2
 from google.appengine.api import taskqueue
 
@@ -15,8 +16,18 @@ class IncomingMailHandler(InboundMailHandler):
 
         if hasattr(mail_message, 'attachments'):
             for filename, content in mail_message.attachments:
+                incoming_filename = filename
+                incoming_content = content.decode()
                 logging.info(filename)
                 logging.info(content.decode())
+
+            mail.send_mail_to_admins(
+                sender='noreply@ios-logs-email.appspotmail.com',
+                subject=incoming_subject + ' logs report',
+                body=incoming_content[:100]
+                )
+
+
 
 
 
